235a3ecbe0ef2c7df1e8a75e48c31955fa8981a8
hitesh1305 <142805472+hitesh1305@users.noreply.github.com>
2025-04-14 02:32:41 +0530

    Update avltree.cpp

--- avltree.cpp
+++ avltree.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Structure to represent a node in AVL Tree
 struct Node {
     int key;
     int height;
@@ -15,22 +14,18 @@ struct Node {
     }
 };
 
-// Function to get height of a node
 int getHeight(Node* node) {
     return node ? node->height : 0;
 }
 
-// Function to get balance factor
 int getBalance(Node* node) {
     return node ? getHeight(node->left) - getHeight(node->right) : 0;
 }
 
-// Function to update height of a node
 void updateHeight(Node* node) {
     node->height = 1 + max(getHeight(node->left), getHeight(node->right));
 }
 
-// Right rotation
 Node* rotateRight(Node* y) {
     Node* x = y->left;
     Node* T2 = x->right;
@@ -44,7 +39,6 @@ Node* rotateRight(Node* y) {
     return x;
 }
 
-// Left rotation
 Node* rotateLeft(Node* x) {
     Node* y = x->right;
     Node* T2 = y->left;
@@ -58,9 +52,8 @@ Node* rotateLeft(Node* x) {
     return y;
 }
 
-// Insert a key into AVL tree
 Node* insert(Node* root, int key) {
-    // Step 1: Normal BST insertion
+  
     if (!root) return new Node(key);
 
     if (key < root->key)
@@ -68,29 +61,24 @@ Node* insert(Node* root, int key) {
     else if (key > root->key)
         root->right = insert(root->right, key);
     else
-        return root; // duplicate keys not allowed
+        return root;
 
-    // Step 2: Update height
     updateHeight(root);
 
-    // Step 3: Balance the tree
     int balance = getBalance(root);
 
-    // Left Left Case
     if (balance > 1 && key < root->left->key)
         return rotateRight(root);
 
-    // Right Right Case
     if (balance < -1 && key > root->right->key)
         return rotateLeft(root);
 
-    // Left Right Case
     if (balance > 1 && key > root->left->key) {
         root->left = rotateLeft(root->left);
         return rotateRight(root);
     }
 
-    // Right Left Case
+ 
     if (balance < -1 && key < root->right->key) {
         root->right = rotateRight(root->right);
         return rotateLeft(root);
@@ -99,7 +87,7 @@ Node* insert(Node* root, int key) {
     return root;
 }
 
-// Inorder traversal (sorted order)
+
 void inorder(Node* root) {
     if (root) {
         inorder(root->left);
